b663d2bf9a74b9b2abfea3157b664131e8863488
Michael <m.zaluckyj@gmail.com>
2022-12-08 16:15:49 -0500

    exercise 1

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+          var celsius = TempConverter.FahrenheitToCelsius(68);
+;               Console.WriteLine($"{celsius}");
+
+            var fahrenheit = TempConverter.CelsiusToFahrenheit(20);
+            Console.WriteLine($"{fahrenheit}");
         }
     }
 }
